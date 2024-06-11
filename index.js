257a09859ffcb775be92bb0aa0fefc0f38f2ba5e
Nrya24 <167544401+Nrya24@users.noreply.github.com>
2024-06-11 23:08:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var question = prompt("Hi Welcome to RUN/HER, 🩷 are you new to running?");
+
+if (question === "yes" || question === "Yes") {
+  alert("Welcome new runner, You have come to the right place!👋");
+} else {
+  alert("Welcome to your new running community 🫶");
+}
---
+++ join-club.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var txt;
+  if (confirm("Thank you for joining!🏃‍♀️")) {
+    txt = "Thank you for joining! 🏃‍♀️";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
+
+addEventListener("DOMContentLoaded", (event) => {});
